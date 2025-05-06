8c51b8c5c7a72bda98d68854fded8647dc251c2d
Cabacon1 <116121814+Cabacon1@users.noreply.github.com>
2025-05-06 13:43:47 -0400

    semi working favorites
    locally storing

--- app.js
+++ app.js
@@ -24,39 +24,104 @@ $.ajax({
                                 <h2>${data[i].title}</h2>
                                 <img src="${data[i].image}" alt="${data[i].title}">
                                 <p>${data[i].description}</p>
-                                <p>Ingredients: ${Array.isArray(data[i].ingredients) ? data[i].ingredients.join(", ") : "N/A"}</p>
+                                <p class="ingredients">Ingredients: ${Array.isArray(data[i].ingredients) ? data[i].ingredients.join(", ") : "N/A"}</p>
                                 <button class="pickFavoriteButton" onclick="pickFavorite(${data[i].id})">Pick as Favorite</button>
                             </div>`;
-        // Append the drink item to the coffeeOptions div
-        // Use jQuery to append the drink item to the coffeeOptions div
-
+        // Append the menu item to the menu list
+        // Use jQuery to append the menu item to the menu list  
         $("#menuList").append(menuItem);
     }
+}).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error: " + textStatus + " " + errorThrown);
+    // Display an error message to the user
+    $("#menuList").html("<p>Sorry, there was an error loading the menu. Please try again later.</p>");
+}
+).always(function () {
+    console.log("Always");
+    // This code will always run, regardless of success or failure
+    // You can use this to hide loading indicators or perform cleanup tasks
+    $("#loading").hide(); // Hide the loading indicator
 });
 
+let favoriteList = [];
 
-let favoritesListDiv = $("#favoritesListDiv");
-// This will be the div that will display the favorites list on the page
-
-favoritesListDiv.hide();
-
-// Add pickFavorite selection to the favorites list
-// This function will take the id of the drink as a parameter and add it to the favorites list
-// The function will also display the favorites list on the page
 function pickFavorite(drinkId) {
-    // Check if the drink is already in the favorites list
-    const drink = favoritesList.find((drink) => drink.id === drinkId);
-    if (drink) {
-        alert("This drink is already in your favorites list.");
+    // Check if the drink is already in the favorite list
+    if (favoriteList.includes(drinkId)) {
+        console.log(`Drink ID ${drinkId} is already in the favorite list.`);
         return;
     }
-    // If the drink is not in the favorites list, add it to the favorites list
-    const newDrink = { id: drinkId, name: `Drink ${drinkId}` };
-    favoritesList.push(newDrink);
-    console.log(favoritesList);
-    // Display the favorites list on the page
-    showFavorites();
+    // Add the drink to the favorite list
+    favoriteList.push(drinkId);
+    console.log(`Drink ID ${drinkId} has been added to the favorite list.`);
+    // Display the updated favorite list
+    displayFavoriteList();
+
+    //Store Favorite List in Local Storage
+    localStorage.setItem("favoriteList", JSON.stringify(favoriteList));
+    // Retrieve Favorite List from Local Storage
+    const storedFavoriteList = localStorage.getItem("favoriteList");
+    if (storedFavoriteList) {
+        favoriteList = JSON.parse(storedFavoriteList);
+    } else {
+        favoriteList = [];
+    }
 }
 
+
+function displayFavoriteList() {
+
+    $("#favoriteList").empty();
+    for (let i = 0; i < favoriteList.length; i++) {
+
+        // Create a favorite item element
+        // Use template literals to create the favorite item element
+        //use the favoriteList[i] to gather the same data as the menuList
+        const favoriteItem = `<div class="favoriteItem"> 
+                                <h2>Favorite Drink ID: ${favoriteList[i]}</h2>
+                                <img src="https://api.sampleapis.com/coffee/iced/${favoriteList[i]}.jpg" alt="Favorite Drink ID: ${favoriteList[i]}">
+                                <p>Drink ID: ${favoriteList[i]}</p>
+                            </div>`;
+
+
+
+        // Append the favorite item to the favorite list
+        // Use jQuery to append the favorite item to the favorite list  
+        $("#favoriteList").append(favoriteItem);
+    }
+    console.log("Favorite List: ", favoriteList);
+    // Display the favorite list in the console
+    console.log("Favorite List: ", favoriteList);
+}
+
+// Add event listener to the button to clear the favorite list
+$("#clearFavoritesButton").on("click", function () {
+    // Clear the favorite list
+    favoriteList = [];
+    // Display the updated favorite list
+    displayFavoriteList();
+});
+
+// Add event listener to the button to load the favorite list from local storage
+$("#loadFavoritesButton").on("click", function () {
+    // Retrieve Favorite List from Local Storage
+    const storedFavoriteList = localStorage.getItem("favoriteList");
+    if (storedFavoriteList) {
+        favoriteList = JSON.parse(storedFavoriteList);
+        // Display the updated favorite list
+        displayFavoriteList();
+    } else {
+        console.log("No favorite list found in local storage.");
+    }
+});
+// Add event listener to the button to clear the favorite list from local storage
+$("#clearFavoritesLocalStorageButton").on("click", function () {
+    // Clear the favorite list from local storage
+    localStorage.removeItem("favoriteList");
+    // Clear the favorite list in the app
+    favoriteList = [];
+    // Display the updated favorite list
+    displayFavoriteList();
+});
 
 
